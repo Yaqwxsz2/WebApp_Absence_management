f7dafeec95b5f1a555c5e0b94068778ef03ef860
Jiahuren <91957550+Jiahuren@users.noreply.github.com>
2022-11-15 19:53:32 +0100

    Update python_shiny.py

--- python_shiny.py
+++ python_shiny.py
@@ -1,4 +1,5 @@
 from shiny import *
+from shiny.ui import tags, h2
 import numpy as np 
 import matplotlib.pyplot as plt
 import pathlib
@@ -15,12 +16,33 @@ numerix_cols = [
 
 
 app_ui = ui.page_fluid(
+    ui.navset_tab(
+        ui.nav('Login', h2('Login'),
+        ui.input_text('y', 'Numero', placeholder='Entrer le numero'),
+    ui.input_password('x', 'Password input', placeholder='Entrer les mots de passes'),
+        ),
+                
+                ui.nav("Professeur", h2("Partie des professeurs"),
     ui.output_table('result'),
+    ),
+
+                ui.nav("Classe", h2("Partie des classes")),
+                ui.nav("Elève", h2("Partie des élèves")),
+                ui.nav('Profil', h2('Profil'),
+                #Voir les informations personnelles + modifier ces informations.
+                ),
+                id="inTabset",
+            ),
+
+
+
+
 )
 
 def server(input, output, session):
     @output
     #Ajouter 
+    
     @render.table
     def result():
         return penguins
